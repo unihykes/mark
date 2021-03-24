2f1c721b81e52ec5f42ba7c8a557aa6e76e18e68
unihykes <33852613@163.com>
2021-03-25 00:49:15 +0800

    添加proto在windows平台下的编译脚本

--- make/end.cmake
+++ make/end.cmake
@@ -67,9 +67,16 @@ ENDIF()
 
 # “¿¿µ protobuf
 IF(${NEED_PROTOBUF})
-    include_directories(${MK_DEPS_PKGS}/protobuf/include)
-    seek_deps_library(${LIBS_PATH} ${MK_DEPS_PKGS}/protobuf/lib64 protobuf)
-    SET(LINK_CUSTOM_LIBS ${LINK_CUSTOM_LIBS} protobuf)
+    IF(WIN32)
+        #todo:
+        #include_directories(${MK_PATH}/protobuf/include)
+        seek_deps_library(${LIBS_PATH} ${MK_DEPS_PKGS}/__build/protobuf/${CMAKE_BUILD_TYPE} protobufd)
+        SET(LINK_CUSTOM_LIBS ${LINK_CUSTOM_LIBS} protobufd)
+    ELSE()
+        include_directories(${MK_DEPS_PKGS}/protobuf/include)
+        seek_deps_library(${LIBS_PATH} ${MK_DEPS_PKGS}/protobuf/lib64 protobuf)
+        SET(LINK_CUSTOM_LIBS ${LINK_CUSTOM_LIBS} protobuf)
+    ENDIF()
 ENDIF()
 
 # “¿¿µ brpc
