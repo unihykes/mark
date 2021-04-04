803ba2b7d1f09d18515b8b11c284bb6154f75bb3
unihykes <33852613@163.com>
2021-04-05 01:50:44 +0800

    添加use_leveldb,作为使用leveldb的示例;

---
+++ src/use_leveldb/use_db.cpp
@@ -0,0 +1,278 @@
+﻿/***************************************************************************************************
+LICENSE:
+    Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+   
+Author:liu.hao(33852613@163.com)
+
+Time:2020-2
+
+info:
+
+***************************************************************************************************/
+#include <markcore.h>
+#include <gtest/gtest.h>
+#include "leveldb/env.h"
+#include "leveldb/db.h"
+#include "leveldb/comparator.h"
+#include "leveldb/write_batch.h"
+
+class use_db : public testing::Test
+{
+protected:
+	static void SetUpTestCase(){}
+	static void TearDownTestCase(){}
+	
+    virtual void SetUp()
+    {
+        leveldb::Options options;
+        
+        //leveldb::Comparator
+        //options.comparator = 默认用字典顺序比较
+        cout<<options.comparator->Name();
+        
+        //create_if_missing
+        options.create_if_missing = true;
+        
+        
+        //error_if_exists,如果为true,则数据库已存在的话,报错
+        options.error_if_exists = false;
+        
+        //paranoid_checks
+        options.paranoid_checks = false;
+        
+        //env
+        options.env = leveldb::Env::Default();
+        
+        //info_log
+        //leveldb::Logger* log = nullptr;
+        //leveldb::Status status = _storageEnv->NewLogger(logfile, &log);
+        options.info_log = nullptr;//使用默认日志文件
+        
+        //write_buffer_size 默认4MB
+        //options.write_buffer_size = 
+        
+        //max_open_files:默认1000
+        //options.max_open_files = 
+        
+        //block_cache
+        //leveldb::Cache 默认NULL,8M缓存
+        //ptions.block_cache = 
+        
+        
+        //block_size 默认4K
+        //ptions.block_size =
+        
+        //block_restart_interval 默认16
+        //ptions.block_restart_interval =
+        
+        //max_file_size 默认2MB
+        //ptions.max_file_size =
+        
+        //compression 默认kSnappyCompression
+        //ptions.compression =
+        
+        //reuse_logs 
+        //ptions.reuse_logs =
+        
+        //filter_policy 默认NULL
+        //ptions.filter_policy =
+        
+        string dbname = "use_leveldb";
+        _status = leveldb::DB::Open(options, dbname, &_db);
+    }
+	
+    virtual void TearDown()
+    {
+        delete _db;
+        _db = nullptr;
+    }
+    
+protected:
+    leveldb::DB*            _db;
+    leveldb::Status         _status;
+};
+
+TEST_F(use_db, Put)
+{
+    leveldb::WriteOptions writeOptions;
+    writeOptions.sync = true;
+    _status = _db->Put(writeOptions, "use_leveldb/1", "true");
+    MK_PRINT("ret=%s", _status.ToString().c_str());
+    
+    writeOptions.sync = false;
+    _status = _db->Put(writeOptions, "use_leveldb/2", "false");
+    MK_PRINT("ret=%s", _status.ToString().c_str());
+}
+
+TEST_F(use_db, Get)
+{
+    leveldb::ReadOptions readOptions;
+    readOptions.verify_checksums = false;
+    readOptions.fill_cache = true;
+    readOptions.snapshot = nullptr;
+    
+    string value;
+    _status = _db->Get(readOptions, "use_leveldb/1", &value);
+    MK_PRINT("ret=%s", _status.ToString().c_str());
+    
+    _status = _db->Get(readOptions, "use_leveldb/2", &value);
+    MK_PRINT("ret=%s", _status.ToString().c_str());
+}
+
+TEST_F(use_db, Delete)
+{
+    leveldb::WriteOptions writeOptions;
+    writeOptions.sync = true;
+    
+    _status = _db->Delete(writeOptions, "use_leveldb/1");
+    MK_PRINT("ret=%s", _status.ToString().c_str());
+    
+    _status = _db->Delete(writeOptions, "use_leveldb/2");
+    MK_PRINT("ret=%s", _status.ToString().c_str());
+}
+
+TEST_F(use_db, Write1)
+{
+    leveldb::WriteOptions writeOptions;
+    writeOptions.sync = true;
+    
+    leveldb::WriteBatch updates;
+    updates.Put("use_leveldb/1", "true");
+    updates.Put("use_leveldb/2", "false");
+    updates.Delete("use_leveldb/1");
+    updates.Clear();
+    updates.Put("use_leveldb/3", "true");
+    updates.Put("use_leveldb/4", "false");
+    updates.Delete("use_leveldb/5");
+    
+    _status = _db->Write(writeOptions, &updates);
+    MK_PRINT("ret=%s", _status.ToString().c_str());
+}
+
+TEST_F(use_db, Write2)
+{
+    class WriteBatchHandler : public leveldb::WriteBatch::Handler
+    {
+        virtual void Put(const leveldb::Slice& key, const leveldb::Slice& value)
+        {
+            MK_PRINT("%s=%s", key.ToString().c_str(), value.ToString().c_str());
+        }
+        virtual void Delete(const leveldb::Slice& key)
+        {
+            MK_PRINT("%s", key.ToString().c_str());
+        }
+    };
+    
+    leveldb::WriteOptions writeOptions;
+    writeOptions.sync = true;
+    
+    leveldb::WriteBatch updates;
+    updates.Put("use_leveldb/1", "true");
+    updates.Put("use_leveldb/2", "false");
+    updates.Delete("use_leveldb/1");
+    updates.Clear();
+    updates.Put("use_leveldb/3", "true");
+    updates.Put("use_leveldb/4", "false");
+    updates.Delete("use_leveldb/5");
+    
+    WriteBatchHandler handler;
+    _status = updates.Iterate(&handler);
+    
+    _status = _db->Write(writeOptions, &updates);
+    MK_PRINT("ret=%s", _status.ToString().c_str());
+}
+
+TEST_F(use_db, NewIterator)
+{
+    leveldb::ReadOptions readOptions;
+    readOptions.verify_checksums = false;
+    readOptions.fill_cache = true;
+    readOptions.snapshot = nullptr;
+    
+    std::shared_ptr<leveldb::Iterator> iter(_db->NewIterator(readOptions));
+    
+    iter->SeekToFirst();
+    while(iter->Valid()) {
+        _status = iter->status();
+        auto key = iter->key();
+        auto value = iter->value();
+        cout<<key.ToString()<<" = "<< value.ToString()<<endl;
+        //MK_PRINT("%s=%s", key.ToString().c_str(), value.ToString().c_str());
+        iter->Next();
+    }
+    
+    iter->SeekToLast();
+    while(iter->Valid()) {
+        _status = iter->status();
+        auto key = iter->key();
+        auto value = iter->value();
+        MK_PRINT("%s=%s", key.ToString().c_str(), value.ToString().c_str());
+        iter->Next();
+    }
+    
+    iter->Seek("use_leveldb/3");
+    while(iter->Valid()) {
+        _status = iter->status();
+        auto key = iter->key();
+        auto value = iter->value();
+        MK_PRINT("%s=%s", key.ToString().c_str(), value.ToString().c_str());
+        iter->Next();
+    }
+    
+    //RegisterCleanup
+    //todo
+}
+
+TEST_F(use_db, readdelete)
+{
+    leveldb::WriteOptions writeOptions;
+    writeOptions.sync = true;
+    
+    string firstkey = "/readdelete/";
+    for(int i = 0; i != 10; ++i) {
+        string curKey1 = firstkey + std::to_string(i) + "/";
+        for(int j = 0; j != 100; ++j) {
+            string curKey = curKey1 + std::to_string(j);
+            _status = _db->Put(writeOptions, curKey, "true");
+        }
+    }
+    leveldb::ReadOptions readOptions;
+    readOptions.verify_checksums = false;
+    readOptions.fill_cache = true;
+    readOptions.snapshot = nullptr;
+    std::shared_ptr<leveldb::Iterator> iter(_db->NewIterator(readOptions));
+    
+    int64 count = 0;
+    iter->Seek(firstkey);
+    while(iter->Valid()) {
+        auto key = iter->key();
+        if (key.starts_with(firstkey) == false) {
+            break;
+        }
+        
+        auto value = iter->value();
+        ++count;
+        cout<<key.ToString()<<" = "<< value.ToString()<<endl;
+        
+        //删除
+        auto status = _db->Delete(writeOptions, key);
+        
+        iter->Next();
+    }
+    MK_PRINT("count = %lld", count);
+}
+
+
+
+
+
---
+++ src/use_leveldb/use_env.cpp
@@ -0,0 +1,316 @@
+﻿/***************************************************************************************************
+LICENSE:
+    Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+   
+Author:liu.hao(33852613@163.com)
+
+Time:2020-2
+
+info:
+
+***************************************************************************************************/
+
+#include <markcore.h>
+#include <gtest/gtest.h>
+#include "leveldb/env.h"
+
+class use_env : public testing::Test
+{
+protected:
+	static void SetUpTestCase()
+    {
+    }
+	static void TearDownTestCase()
+    {
+    }
+	
+    virtual void SetUp()
+    {
+        _env = leveldb::Env::Default();
+    }
+	
+    virtual void TearDown()
+    {
+        _env = nullptr;
+    }
+	
+    void Logv(leveldb::Logger* log, const char* fmt, ...)
+    {
+        va_list args;
+        va_start(args, fmt);
+        
+        log->Logv(fmt, args);
+        
+        va_end (args);
+    }
+protected:
+    leveldb::Env*            _env;
+};
+
+TEST_F(use_env, common)
+{
+	
+    std::string fname = "use_leveldb.config";
+    std::string dir = "use_leveldb_dir";
+    bool ret = false;
+    leveldb::Status status;
+    
+    //FileExists
+    {
+        ret = _env->FileExists(fname);
+        MK_PRINT("ret = %d", int(ret));
+    }
+    
+    //GetChildren
+    {
+        std::vector<std::string> result;
+        status = _env->GetChildren(dir, &result);
+        
+        for(auto& elem : result) {
+            MK_PRINT("ret = %s", elem.c_str());
+        }
+    }
+    
+    //DeleteFile
+    {
+        string todo = "DeleteFile.txt";
+        status = _env->DeleteFile(todo);
+        MK_PRINT("ret = %d", int(ret));
+    }
+    
+    //CreateDir && DeleteDir
+    {
+        string todo = "CreateDir.txt";
+        status = _env->CreateDir(todo);
+        MK_PRINT("ret = %d", int(ret));
+        
+        status = _env->DeleteDir(todo);
+        MK_PRINT("ret = %d", int(ret));
+    }
+    
+    //GetFileSize
+    {
+        uint64_t file_size = 0;
+        status = _env->GetFileSize(fname, &file_size);
+        MK_PRINT("file_size = %lld", file_size);
+    }
+    
+    //RenameFile
+    {
+        string src = fname;
+        string target = "rename.txt";
+        
+        status = _env->RenameFile(fname, target);
+        MK_PRINT("ret = %s", status.ToString().c_str());
+        
+        status = _env->RenameFile(target, src);
+        MK_PRINT("ret = %s", status.ToString().c_str());
+    }
+    
+    //LockFile&&UnlockFile
+    {
+        leveldb::FileLock* lock = nullptr;
+        
+        status = _env->LockFile(fname, &lock);
+        MK_PRINT("ret = %s", status.ToString().c_str());
+        
+        status = _env->UnlockFile(lock);
+        MK_PRINT("ret = %s", status.ToString().c_str());
+    }
+    
+    //Schedule
+    {
+        //todo
+    }
+    
+    //StartThread
+    {
+        //todo
+    }
+    
+    //GetTestDirectory
+    {
+        std::string path;
+        status = _env->GetTestDirectory(&path);
+        //C:\Users\ADMINI~1\AppData\Local\Temp\ 
+        MK_PRINT("ret = %s", path.c_str());
+    }
+    
+    //NowMicros
+    {
+        uint64_t micros = 0;
+        micros = _env->NowMicros();
+        MK_PRINT("micros = %lld", micros);
+    }
+    
+    //SleepForMicroseconds
+    {
+        //1微秒=百万分之一秒
+        int micros = 1000*1000;
+        _env->SleepForMicroseconds(micros);
+    }
+}
+
+//日志
+TEST_F(use_env, NewLogger)
+{
+    //NewLogger
+    {
+        string logfile("use_leveldb.log");
+        leveldb::Logger* log = nullptr;
+        
+        leveldb::Status status = _env->NewLogger(logfile, &log);
+        MK_PRINT("ret = %s", status.ToString().c_str());
+        
+        //use_env::Logv
+        Logv(log, "just for test: %s, %d...", "xxx", 123);
+        
+        
+        //全局函数::leveldb::Log
+        leveldb::Log(log, "just for test22222222: %s, %d...", "fafaefe", 467);
+        
+    }
+}
+
+//顺序读, 单线程使用
+TEST_F(use_env, NewSequentialFile)
+{
+    std::string fname = "use_leveldb.config";
+    leveldb::Status status;
+    leveldb::SequentialFile* file;
+    status = _env->NewSequentialFile(fname, &file);
+    MK_PRINT("ret = %s", status.ToString().c_str());
+    if(!status.ok()) {
+        return;
+    }
+    
+    
+    //Read
+    for(;;) {
+        //一次最多读取n个字节
+        const size_t n = 2;
+		char scratch[n+1]; memset(scratch,0,n+1); scratch[n] = '\0';
+        leveldb::Slice result;
+        status = file->Read(n, &result, scratch);
+        cout <<"ret = " <<status.ToString()<<endl;
+        MK_PRINT("ret = %s", result.ToString().c_str());
+        if(result.empty()) {
+            break;
+        }
+        cout<<scratch<<endl;
+        
+        //跳过x个字节
+        uint64_t x = 1;
+        status = file->Skip(x);
+        MK_PRINT("ret = %s", status.ToString().c_str());
+    }
+}
+
+//随机读, 可多线程使用
+TEST_F(use_env, NewRandomAccessFile)
+{
+    std::string fname = "use_leveldb.config";
+    leveldb::Status status;
+    leveldb::RandomAccessFile* file;
+    status = _env->NewRandomAccessFile(fname, &file);
+    MK_PRINT("ret = %s", status.ToString().c_str());
+    if(!status.ok()) {
+        return;
+    }
+    
+    //Read
+    uint64_t offset = 0;
+    for(;;) {
+		constexpr size_t n = 2;
+		char scratch[n+1]; memset(scratch,0,n+1); scratch[n] = '\0';
+        leveldb::Slice result;
+        status = file->Read(offset, n, &result, scratch);
+		cout <<"ret = " <<status.ToString()<<endl;
+        MK_PRINT("ret = %s", result.ToString().c_str());
+        if(result.empty()) {
+            break;
+        }
+        
+        offset += n;
+    }
+}
+
+//顺序写,单线程使用,如果文件已存在,会清空文件
+TEST_F(use_env, NewWritableFile)
+{
+    std::string fname = "use_leveldb.config";
+    leveldb::Status status;
+    leveldb::WritableFile* file;
+    status = _env->NewWritableFile(fname, &file);
+    MK_PRINT("ret = %s", status.ToString().c_str());
+    if(!status.ok()) {
+        return;
+    }
+    
+    for(int i = 0; i != 5; ++i) {
+        for(int j = 0; j != 10; ++j) {
+            file->Append("juest for a test !!!!\n");//追加写
+        }
+        file->Flush();//调用WriteFile写入磁盘
+    }
+    file->Sync();//Sync内部包含Flush操作,Flush之后.调用FlushFileBuffers函数, 保证在文件关闭之前所有的调用WriteFile写入磁盘的数据真正被写入磁盘
+    file->Close();//Close内部包含Flush操作, Flush之后关闭文件
+}
+
+
+//顺序写,单线程使用,如果文件已存在,会追加写
+TEST_F(use_env, NewAppendableFile)
+{
+    std::string fname = "use_leveldb.config";
+    leveldb::Status status;
+    leveldb::WritableFile* file;
+    status = _env->NewAppendableFile(fname, &file);
+    MK_PRINT("ret = %s", status.ToString().c_str());
+    if(!status.ok()) {
+        return;
+    }
+    
+    for(int i = 0; i != 5; ++i) {
+        for(int j = 0; j != 10; ++j) {
+            file->Append("juest for a test !!!!\n");//追加写
+        }
+        file->Flush();//调用WriteFile写入磁盘
+    }
+    file->Sync();//Sync内部包含Flush操作,Flush之后.调用FlushFileBuffers函数, 保证在文件关闭之前所有的调用WriteFile写入磁盘的数据真正被写入磁盘
+    file->Close();//Close内部包含Flush操作, Flush之后关闭文件
+}
+
+//全局函数
+TEST_F(use_env, ReadWrite)
+{
+    leveldb::Status status;
+    std::string fname = "use_leveldb.config";
+    
+    //write,如果原文件有内容,清空之
+    leveldb::Slice data("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+    status = leveldb::WriteStringToFile(_env, data, fname);
+    
+    //read
+    string buf;
+    status = leveldb::ReadFileToString(_env, fname, &buf);
+}
+
+
+
+
+
+
+
+
+
+
